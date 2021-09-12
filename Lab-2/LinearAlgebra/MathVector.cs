7e787eaad1f4c887a6413b6143550f4327cac23a
Vadim <Vadim2003Rus@yandex.ru>
2021-09-12 23:00:29 +0300

    Вторая лабораторная

---
+++ Lab-2/LinearAlgebra/MathVector.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinearAlgebra
+{
+    public class MathVector : IMathVector
+    {
+        private List<double> _list;
+
+        public MathVector(IEnumerable<double> arr)
+        {
+            this._list = arr.ToList();
+        }
+
+        public double this[int i] 
+        {
+            get
+            {
+                return _list[i];
+            }
+            set
+            {
+                _list[i] = value;
+            }
+        }
+
+        public int Dimensions 
+        {
+            get { return _list.Count; }
+        }
+
+        public double Length {
+            get
+            {
+                return Math.Sqrt(_list.Aggregate((acc, x) => acc + x * x));
+            } 
+        }
+
+        public double CalcDistance(IMathVector vector)
+        {
+            return Math.Sqrt(
+                _list.Select((x, inx) => Math.Pow(x - vector[inx], 2))
+                .Aggregate((acc, x) => acc + x)
+            );
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        public IMathVector Multiply(IMathVector vector)
+        {
+            return new MathVector(_list.Select((x, inx) => x * vector[inx]));
+        }
+
+        public IMathVector MultiplyNumber(double number)
+        {
+            return new MathVector( _list.Select(x => x * number) );
+        }
+
+        public double ScalarMultiply(IMathVector vector)
+        {
+            return _list.Select((x, inx) => x * vector[inx])
+                .Aggregate((acc, x) => acc + x);
+        }
+
+        public IMathVector Sum(IMathVector vector)
+        {
+            return new MathVector(_list.Select((x, inx) => x + vector[inx]));
+        }
+
+        public IMathVector Subtract(IMathVector vector)
+        {
+            return new MathVector(_list.Select((x, inx) => x + vector[inx]));
+        }
+
+        public IMathVector SumNumber(double number)
+        {
+            return new MathVector(_list.Select(x => x + number));
+        }
+
+        public static IMathVector operator + (MathVector vector1, MathVector vector2)
+        {
+            return vector1.Sum(vector2);
+        }
+
+        public static IMathVector operator +(MathVector vector1, double number)
+        {
+            return vector1.SumNumber(number);
+        }
+
+        public static IMathVector operator -(MathVector vector1, MathVector vector2)
+        {
+            return vector1.Subtract(vector2) ;
+        }
+
+        public static IMathVector operator -(MathVector vector1, double number)
+        {
+            return vector1.SumNumber(-number);
+        }
+
+        public static IMathVector operator *(MathVector vector1, MathVector vector2)
+        {
+            return vector1.Multiply(vector2);
+        }
+
+        public static IMathVector operator *(MathVector vector1, double number)
+        {
+            return vector1.MultiplyNumber(number);
+        }
+
+        public static IMathVector operator /(MathVector vector1, double number)
+        {
+            return vector1.MultiplyNumber(1/number);
+        }
+
+        public static double operator %(MathVector vector1, MathVector vector2)
+        {
+            return vector1.ScalarMultiply(vector2);
+        }
+    }
+}
---
+++ Lab-2/VectorDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using LinearAlgebra;
+
+namespace VectorDemo
+{
+    public static class MathVectorExtension
+    {
+        public static void Display(this IMathVector vector)
+        {
+            foreach (double value in vector)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMathVector vector1 = new MathVector(new List<double>() { 1, 2, 3 });
+            
+
+            Console.WriteLine($"Demensions {vector1.Dimensions}");
+
+            Console.WriteLine($"Lenght {vector1.Length}");
+
+            IMathVector result1 =  vector1.MultiplyNumber(10);
+            result1.Display();
+
+
+            IMathVector vector2 = new MathVector(new List<double>() { 2, 3, 4 });
+
+            IMathVector result2 = vector1.Multiply(vector2);
+            result2.Display();
+
+
+
+
+        }
+    }
+}
